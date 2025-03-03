8b49b4db5293c15d983bf8c0ebc438fc92071f6d
Pedro Augusto <pcarreteiro01@gmail.com>
2025-03-03 14:58:28 -0300

    Produtos OK início Footer

--- scriptdestaques.js
+++ scriptdestaques.js
@@ -1,36 +1,36 @@
-        const imagesFolder = 'imagens/'; // Caminho da pasta onde estão as imagens
-        const images = [
-            imagesFolder + 'PuffleVerde.webp', imagesFolder + 'PuffleRoxo.webp', imagesFolder + 'PuffleRosa.webp', imagesFolder + 'PufflePreto.webp',
-            imagesFolder + 'PuffleAzul.webp', imagesFolder + 'Puffle_vermelho.webp', imagesFolder + 'Puffle_laranja.webp', imagesFolder + 'Puffle_colorido.webp'
-        ];
+//         const imagesFolder = 'imagens/'; // Caminho da pasta onde estão as imagens
+//         const images = [
+//             imagesFolder + 'PuffleVerde.webp', imagesFolder + 'PuffleRoxo.webp', imagesFolder + 'PuffleRosa.webp', imagesFolder + 'PufflePreto.webp',
+//             imagesFolder + 'PuffleAzul.webp', imagesFolder + 'Puffle_vermelho.webp', imagesFolder + 'Puffle_laranja.webp', imagesFolder + 'Puffle_colorido.webp'
+//         ];
         
-let currentIndex = 0;
-const imagesPerPage = 4;
+// let currentIndex = 0;
+// const imagesPerPage = 4;
 
-function showImages() {
-    const container = document.getElementById('imageContainer');
-    container.innerHTML = '';
-    for (let i = currentIndex; i < currentIndex + imagesPerPage; i++) {
-        if (images[i]) {
-            const img = document.createElement('img');
-            img.src = images[i];
-            container.appendChild(img);
-        }
-    }
-}
+// function showImages() {
+//     const container = document.getElementById('imageContainer');
+//     container.innerHTML = '';
+//     for (let i = currentIndex; i < currentIndex + imagesPerPage; i++) {
+//         if (images[i]) {
+//             const img = document.createElement('img');
+//             img.src = images[i];
+//             container.appendChild(img);
+//         }
+//     }
+// }
 
-function prevSlide() {
-    if (currentIndex > 0) {
-        currentIndex -= imagesPerPage;
-        showImages();
-    }
-}
+// function prevSlide() {
+//     if (currentIndex > 0) {
+//         currentIndex -= imagesPerPage;
+//         showImages();
+//     }
+// }
 
-function nextSlide() {
-    if (currentIndex + imagesPerPage < images.length) {
-        currentIndex += imagesPerPage;
-        showImages();
-    }
-}
+// function nextSlide() {
+//     if (currentIndex + imagesPerPage < images.length) {
+//         currentIndex += imagesPerPage;
+//         showImages();
+//     }
+// }
 
-showImages();
+// showImages();
